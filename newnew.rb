a13d7b6af5a958adc57939c18b21b2de3ea5c826
CarlosOrdonez <54687569+CarlosOrdonez@users.noreply.github.com>
2020-01-31 14:47:08 -0800

    Add files via upload

---
+++ newnew.rb
@@ -0,0 +1,103 @@
+use_bpm 129
+sample "C:/Users/carlos_ordonez/Downloads/challenge_d/travis_sounds/khaled1.wav", amp: 4
+dj = "C:/Users/carlos_ordonez/Downloads/challenge_d/travis_sounds/boiiii.wav"
+sleep 8
+
+live_loop:s do
+  sample dj
+  sleep 8
+end
+
+live_loop:reel do
+  play :e4
+  play :g4
+  play:c3
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :e4
+  play :g4
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :e4
+  play :g4
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :e4
+  play :g4
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :e4
+  play :g4
+  play :c3
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :e4
+  play :g4
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :e4
+  play :a4
+  play :a3
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :e4
+  play :g4
+  play :g3
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :g4
+  play :d4
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :g4
+  play :d4
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :g4
+  play :d4
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :g4
+  play :d4
+  play :b2
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :g4
+  play :d4
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :a4
+  play :d4
+  play :a3
+  sleep 0.5
+  play :c4
+  sleep 0.5
+  play :g4
+  play :d4
+  play :g3
+  sleep 0.5
+  play :c4
+  sleep 0.5
+end
+
+
+
+
+
+
+
+
+
